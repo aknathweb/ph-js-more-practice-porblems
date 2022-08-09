d0ea51a30171889fc53e8ab4d158967bad9f8774
aknathweb <aniknwu.cse@gmail.com>
2022-08-10 00:00:30 +0600

    first problem solve

--- p-1.js
+++ p-1.js
@@ -3,3 +3,7 @@
 // shopkeeper will return. The total cost of 1 kg of oranges and 1 kg of
 // apples is tk 700.
 
+const harryMomTK = 1000;
+const purchaseTK = 700;
+const change = harryMomTK - purchaseTK;
+console.log("shopkeeper will return : ", change);
