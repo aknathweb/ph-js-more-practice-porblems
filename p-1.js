8d45f8361056909809cba8e00eeefb4a0043102c
aknathweb <aniknwu.cse@gmail.com>
2022-08-09 23:56:23 +0600

    more practice porblems initial stage

---
+++ p-1.js
@@ -0,0 +1,5 @@
+// 1) Harry’s mom gave him tk 1000 and asked him to buy some oranges and
+// apples. Write a program to help Harry calculate how much money the
+// shopkeeper will return. The total cost of 1 kg of oranges and 1 kg of
+// apples is tk 700.
+
