c43d44a2483238f7c7d33876377d78cfb0c9b44d
aknathweb <aniknwu.cse@gmail.com>
2022-08-10 00:44:50 +0600

    find out reminder value

---
+++ p-4.js
@@ -0,0 +1,8 @@
+// 4) Sarah’s mother is teaching her mathematics. She gave Sarah the number
+// 119 and asked her what the remainder would be if she divided the number
+// by 5. Help Sarah write the program.
+
+const number = 119;
+const divider = 5;
+const remainder = 119 % 5;
+console.log("calculate reminder : ", remainder);
