0ad0c62aaa57200c269f8b38d7bdec5773cfaefc
aknathweb <aniknwu.cse@gmail.com>
2022-08-10 00:37:10 +0600

    average mark

---
+++ p-2.js
@@ -0,0 +1,6 @@
+// 2) Write a program to calculate the average marks of Mathematics,
+// Biology, Chemistry, Physics, and Bangla of a student.
+
+const Biology = 70, Chemistry = 50, Physics = 65, Bangla = 88;
+const average = (Biology + Chemistry + Physics + Bangla) / 4;
+console.log("average marks : ", average.toFixed(3));
