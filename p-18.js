631e474d711f3aacc3e6eb85a9f1b4149fa325c5
aknathweb <aniknwu.cse@gmail.com>
2022-08-12 01:01:57 +0600

    solution of problem 18

---
+++ p-18.js
@@ -0,0 +1,5 @@
+// 18) while লপু কি ভাবে কাজ করে?
+
+/*
+In while loop, condition is evaluated first and if it returns true then the statements inside while loop execute, this happens repeatedly until the condition returns false.When condition returns false, the control comes out of loop and jumps to the next statement in the program after while loop.
+*/
