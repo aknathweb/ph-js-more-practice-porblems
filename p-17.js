c51af42abc43e564c7786251abd4ac690c32a15b
aknathweb <aniknwu.cse@gmail.com>
2022-08-12 01:01:04 +0600

    solution of problem 17

---
+++ p-17.js
@@ -0,0 +1,6 @@
+// 17) আসকে আমার মন ভালো নেই এই কথা ৩৯ বার আউটপুট হিসেবে দেখাও।
+const times = 39;
+let time = times;
+while (time--) {
+    console.log(times - time, "->", "আসকে আমার মন ভালো নেই");
+}
