c8c6cdd464247ec485e2ca0c5cb8c130d3c60508
aknathweb <aniknwu.cse@gmail.com>
2022-08-12 21:16:47 +0600

    done problem 20 solution

---
+++ p-20.js
@@ -0,0 +1,7 @@
+// 20) একটা কোড লিখে ৪১২ থেকে ৪৫৬ পযন্ত যত জোর সংখ্যা আছে সেগুলাকে
+// দেখাও
+
+for (let number = 412; number <= 456; number++) {
+    if (number % 2 == 0)
+        console.log("Even number: ", number);
+}
