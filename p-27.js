8e34d38f6f2829dd8889a4ecc4a6ddf856ac67ec
aknathweb <aniknwu.cse@gmail.com>
2022-08-15 12:27:39 +0600

    done problem 27

---
+++ p-27.js
@@ -0,0 +1,7 @@
+// 27) Write a function called make_avg() which will take an three integers and
+// return the average of those values.
+function make_avg(first, second, third) {
+    const average = (first + second + third) / 3;
+    return average;
+}
+console.log(2, 3, 4);
