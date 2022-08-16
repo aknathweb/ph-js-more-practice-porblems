753fb4d194bf3517a495f98153f2e1c0ca863dbc
aknathweb <aniknwu.cse@gmail.com>
2022-08-16 09:04:47 +0600

    done porblem 32

---
+++ p-32.js
@@ -0,0 +1,7 @@
+// 32) একটা ফাংশন লিখবা যেটা যেকোন নামকে uppercase বা regularcase হিসেবে নিবে আর
+// আউটপুট হিসেবে সেই নাম lowercase করে রিটার্ন করবে।
+
+function lowercaseText(text) {
+    return text.toLowerCase();
+}
+console.log(lowercaseText('Anik Kumar Nath'));
