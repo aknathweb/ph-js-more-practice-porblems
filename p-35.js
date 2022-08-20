5ba531cfdf482f85b0875c759f15cae8ab2ecf53
aknathweb <aniknwu.cse@gmail.com>
2022-08-20 14:11:46 +0600

    done problem 35

---
+++ p-35.js
@@ -0,0 +1,6 @@
+// 35) Write a function that will take hour as the input parameter and will convert it
+// into minutes and will return the result in minutes.
+function hourtominute(hour) {
+    return hour * 60;
+}
+console.log(hourtominute(2));
