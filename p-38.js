8fa4397ec6ebb84795f804b4626e9fcdd3a9b9fd
aknathweb <aniknwu.cse@gmail.com>
2022-08-20 14:54:11 +0600

    done p-38

---
+++ p-38.js
@@ -0,0 +1,9 @@
+// 38) leapYear() নামে ফাংশন লিখো এবং নেক্সট ইয়ার অর্থাৎ ২০২৩ কি leap year নাকি সেটা
+// চেক করো। leap year হলে ফাংশন true রিটার্ন করবে আর leap year না হলে false রিটার্ন
+
+function leapYear(year) {
+    if ((year % 400 == 0 || year % 4 == 0) && (year % 100 != 0))
+        return true;
+    return false;
+}
+console.log(leapYear(2044));
