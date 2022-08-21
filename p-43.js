b35449c63c7f9e26ab93489cca6737ac0bc96f97
aknathweb <aniknwu.cse@gmail.com>
2022-08-21 09:57:04 +0600

    done problem 43

---
+++ p-43.js
@@ -0,0 +1,6 @@
+// 43) তিনটা সংখ্যা এর যোগ করতে পারবে এমন একটা ফাংশন লিখো
+const number1 = 3, number2 = 6, number3 = 5;
+function addThree(number1, number2, number3) {
+    return number1 + number2 + number3;
+}
+console.log(addThree(number1, number2, number3));
