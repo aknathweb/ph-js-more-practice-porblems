6af54ac54c1694060445b57b80df86e0e7b765c0
aknathweb <aniknwu.cse@gmail.com>
2022-08-21 10:15:13 +0600

    done problem 44

---
+++ p-44.js
@@ -0,0 +1,7 @@
+// ১. তোমাকে ফাংশনের ইনপুট হিসেবে সেলসিয়াস দিবে। তুমি ক্যালকুলেশন করে তাপমাত্রা ফারেনহাইট এ কনভার্ট করে সেটার আউটপুট রিটার্ন করবে
+const CelsiusTemperature = 20;
+function CelsiusToFahrenheit(Celsius) {
+    const Fahrenheit = ((9 / 5) * Celsius) + 32;
+    return Fahrenheit;
+}
+console.log(CelsiusToFahrenheit(CelsiusTemperature));
