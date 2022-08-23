0374167718199950fcacec3e9daf507cc591ad94
aknathweb <aniknwu.cse@gmail.com>
2022-08-23 22:06:08 +0600

    done problem 47 compound  interest calculation part

--- p-47.js
+++ p-47.js
@@ -14,4 +14,17 @@ const Year = 2;
 const SimpleInterest = Asset * Year * InterestRate / 100;
 console.log("Simple Interest", SimpleInterest);
 
-
+// Question 2:
+// বার্ষিক 5% হারে 10000 টাকার 2 বছরের চক্রবৃদ্ধি সুদ কত টাকা?
+// আমরা জানি যে, সুদ-আসল = P(1 + r/100)^t
+// সুদ-আসল = আসল + চক্রবৃদ্ধি সুদ 
+// অতএব, 
+// সুদ-আসল = 10000 (1 + 5/100)^2 
+// সুদ-আসল = 11025 
+// চক্রবৃদ্ধি সুদ = 11025 - 10000 = 1025
+const Asset1 = 10000;
+const InterestRate1 = 5;
+const Year1 = 2;
+const InterestPrincipal1 = Asset1 * Math.pow((1 + InterestRate1 / 100), Year1);
+const CompoundInterest = InterestPrincipal1 - Asset1;
+console.log("Compound Interest", CompoundInterest);
