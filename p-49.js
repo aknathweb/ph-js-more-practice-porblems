3922b410aef068fcf54e529bc2a66c81ef46773f
aknathweb <aniknwu.cse@gmail.com>
2022-08-24 20:45:55 +0600

    done problem 49

--- p-49.js
+++ p-49.js
@@ -1,1 +1,12 @@
 
+// 49) Write a function and take an array as a parameter. Find the average of all
+// the elements of that array and return this average.
+
+const numbers = [5, 6, 8, 3, 9, 1, 3, 5, 7, 2];
+function average(numbers) {
+    let sum = 0;
+    for (const number of numbers)
+        sum += number;
+    return sum / numbers.length;
+}
+console.log(average(numbers));
