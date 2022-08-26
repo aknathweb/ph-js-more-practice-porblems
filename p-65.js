97e543ac928274f5e0ce3ebd35f24ec58157f109
aknathweb <aniknwu.cse@gmail.com>
2022-08-26 17:34:11 +0600

    done problem 65

---
+++ p-65.js
@@ -0,0 +1,6 @@
+// 65) তোমাকে যদি বলা হয়। একটা array এর মধ্যে ৮০ এর চাইতে বড় সংখ্যা খাকলে সেগুলাকে
+// console log  করে দেখাতে সেটা কি তুমি পারবে? তাহলে তুমি সেই কোড করে ফেলো
+const array = [90, 45, 89, 84, 98, 100, 56, 67];
+for (const element of array)
+    if (element > 80)
+        console.log(element);
