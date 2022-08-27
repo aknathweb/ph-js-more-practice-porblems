942980c7181870e32ce95667a2d811b383b15dbe
aknathweb <aniknwu.cse@gmail.com>
2022-08-27 10:59:27 +0600

    done problem 67

---
+++ p-67.js
@@ -0,0 +1,10 @@
+// 67) একটা অবজেক্ট ডিক্লেয়ার করবে। সেটাতে তিনটা প্রপার্টি খাকবে। এবং কোন একটা প্রোপার্টি
+// এর মান চেইঞ্জ করবা।
+
+const object = {
+    First_name: "Anik",
+    middle_name: "Kumar",
+    last_name: "Nath"
+}
+object.First_name = 'Kanak';
+console.log(object.First_name);
